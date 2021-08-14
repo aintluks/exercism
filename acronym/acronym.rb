355ccff86c87eed713db927b87e11b9617d31a4e
Lucas da Costa <aintluks@gmail.com>
2021-08-14 10:53:03 -0300

    Acronym: refactoring

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -1,13 +1,8 @@
 class Acronym
-  def self.in_caracter(string)
-    string.split(' ').map { |s| s.split('-').map { |s1| s1.chars.first if s1.chars != '-' } }.join.upcase
-  end
-
-  def self.not_in_caracter(string)
-    string.split(' ').map { |s| s.chars.first }.join.upcase
-  end
-
   def self.abbreviate(string)
-    string.include?('-') ? in_caracter(string) : not_in_caracter(string)
+    string.split(/(\s|-)/)
+      .select { |s| s != ' ' && s != '-' && !s.empty? }
+      .map { |s| s.split('').first.upcase }
+      .join
   end
 end
