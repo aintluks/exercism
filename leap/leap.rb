44df039c21b03f75b6027e7d87e51955c2724de1
Lucas da Costa <aintluks@gmail.com>
2021-08-14 10:06:49 -0300

    Leap: refactoring

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,18 +1,5 @@
 class Year
   def self.leap?(year)
-    divisible_4 = year % 4 == 0
-    divisible_100 = year % 100 == 0
-    divisible_400 = year % 400 == 0
-    is_leap = true
-
-    if divisible_4
-      if (not divisible_400) && divisible_100
-        is_leap = false
-      end
-    else
-      is_leap = false
-    end
-    
-    is_leap
+    year % 4 == 0 and (not year % 100 == 0) or year % 400 == 0
   end
 end
